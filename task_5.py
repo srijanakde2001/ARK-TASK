01bebfbbddea3764ecbdb12f2aa26b56d6b0b328
Srijanak De <54339186+srijanakde2001@users.noreply.github.com>
2020-03-02 14:52:26 +0530

    m1

--- task_5.py
+++ task_5.py
@@ -7,13 +7,3 @@ from mpl_toolkits.mplot3d import Axes3D
 data = pd.read_excel('C:/Users/saume/Downloads/radar_dump.xlsx')
 data = data.fillna(value = 0)
 print(data)
-xs = []
-ys = []
-zs = []
-
-
-
-fig = plt.figure
-ax = plt.axes(projection="3d")
-ax.scatter(xs = xs, ys = ys, zs = zs, zdir='z', s=20, c=None, depthshade=True)
-plt.show()
